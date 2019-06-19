5b53387935930c73910f2b95c6ac1083f286d9c3
ietheredge <rianetheredge@gmail.com>
2019-06-19 18:09:11 +0200

    ...

--- vae_quant.py
+++ vae_quant.py
@@ -432,8 +432,8 @@ def main():
             if utils.isnan(obj).any():
                 raise ValueError('NaN spotted in objective.')
             obj.mean().mul(-1).backward()
-            print(elbo.mean().data[0].item())
-            elbo_running_mean.update(elbo.mean().data[0].item())
+            print(elbo.mean().data)
+            elbo_running_mean.update(elbo.mean().data)
             optimizer.step()
 
             # report training diagnostics
