8b5cf142b812500967e4b80317eb3faa88a5fd2c
ietheredge <rianetheredge@gmail.com>
2019-06-23 00:20:25 +0200

    ...

--- vae_quant.py
+++ vae_quant.py
@@ -438,8 +438,8 @@ def display_samples(model, x, save, epoch=0, n_trv_exmp=3, n_trv_stps=10, min_tr
 
 
 def plot_elbo(train_elbo, save, epoch):
-    plt.figure(figsize=(10,10))
-    plt.plot(train_elbo)
+    plt.figure(figsize=(10, 10))
+    plt.plot(train_elbo.detach().cpu().numpy())
     fig = plt.gcf()
     fig.savefig(os.path.join(save, 'elbo_{}.pdf'.format(epoch)))
 
